ef565c8edfecc7ccef475b553613af131d2c9049
Ines HAMIDI <ineshamid@gmail.com>
2023-03-10 13:42:33 +0100

    use refresh token to generate tokens

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -8,16 +8,20 @@ import {
 import { Observable } from 'rxjs';
 import { User } from '../interfaces/user';
 import { Token } from '../interfaces/token';
+import { UserService } from '../services/user.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
-  constructor() {}
+ 
+  constructor(private us: UserService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     console.log(request)
     if (request.url != 'http://localhost:8080/ws/token') {
       const tokens: Token = JSON.parse(localStorage.getItem('tokens') ?? "")
+      if(this.us.isExpiredToken(tokens.accessToken!)){
+        //les tokens ont expiré donc faut régénérer une nouvelle paire à partir de refreshToken
+      }
       request = request.clone({
         setHeaders: {
           'Authorization': 'Bearer ' + tokens.accessToken
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -15,14 +15,20 @@ export class UserService {
     return this.http.post<Token>(this.url, user);
   }
 
-  getUsernameFromToken(token: string)  {
-    const decoded: { sub: string} = jwt_decode(token)
+  getUsernameFromToken(token: string) {
+    const decoded: { sub: string } = jwt_decode(token)
     return decoded.sub
   }
 
-  isExpiredToken(token: string){
-    const decoded: {exp: number} = jwt_decode(token);
+  isExpiredToken(token: string): boolean {
+    const decoded: { exp: number } = jwt_decode(token);
     const { exp } = decoded; //en gros on crée une variable exp qui contient la valeur de l'attribut exp du jeton
+    return exp * 1000 < Date.now();
+  }
+
+  generateTokenFromRefreshToken(token: string) {
+    const user: User = { grantType: "refreshToken", refreshToken: token };
+    return this.http.post<Token>(this.url, user);
   }
 }
 
